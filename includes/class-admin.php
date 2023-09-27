168caedfda574b3b30409f39ee681a45adcbcb8d
vlada-kjero <vlada.vojnovic@kjero.com>
2023-09-27 20:27:24 +0200

    fix: manage network and make js use for multiple tables

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -26,10 +26,10 @@ class Admin {
 		add_action( 'load-users.php', array( $this, 'on_user_page_init' ) );
 
 		// Site admin user add table manage site
-		add_action( 'edit_user_profile', array( $this, 'manage_site_options' ), 1 );
-		add_action( 'show_user_profile', array( $this, 'manage_site_options' ), 1 );
-		add_action( 'personal_options_update', array( $this, 'manage_site_options_update' ) );
-		add_action( 'edit_user_profile_update', array( $this, 'manage_site_options_update' ) );
+		add_action( 'edit_user_profile', array( $this, 'manage_user_section' ), 1 );
+		add_action( 'show_user_profile', array( $this, 'manage_user_section' ), 1 );
+		add_action( 'personal_options_update', array( $this, 'manage_user_section_update' ) );
+		add_action( 'edit_user_profile_update', array( $this, 'manage_user_section_update' ) );
 
 		// add js file
 		add_action( 'admin_enqueue_scripts', array( $this, 'table_enqueue_admin_script' ) );
@@ -39,95 +39,143 @@ class Admin {
 		wp_enqueue_script( 'script_table', plugin_dir_url( __FILE__ ) . 'js/table.js', array(), '1.0' );
 	}
 
+	public function manage_user_section( $user ) {
+		if ( user_can( $user->ID, 'manage_network_users' ) ) {
+			$this->manage_site_options( $user );
+			$this->manage_network_options( $user );
+		}
+	}
+
 	/**
 	 * @return void
 	 */
-	public function manage_site_options( $user ) {
-		if ( ! user_can( $user->ID, 'manage_network_users' ) ) {
-			return;
-		}
+	protected function manage_site_options( $user ) {
 		$all_sites         = get_sites();
 		$current_user_site = wp_sub_get_user_sites( $user->ID );
-		$data_site_format  = array();
 		?>
-		<h2><?php _e( 'Manage Sites', 'wp-separate-user-base' ); ?></h2>
+		<h2><?php esc_html_e( 'Manage Sites', 'wp-separate-user-base' ); ?></h2>
+		<?php
+		foreach ( $all_sites as $site ) {
+			$site->site_name = $site->blogname;
+			$site->id        = $site->blog_id;
+		}
+		$this->create_table( 'site_id', $all_sites, $current_user_site );
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function manage_network_options( $user ) {
+		$all_networks         = get_networks();
+		$current_user_network = get_user_meta( $user->ID, WP_Separate_User_Base::NETWORK_META_KEY, false );
+		?>
+		<h2><?php esc_html_e( 'Manage Networks', 'wp-separate-user-base' ); ?></h2>
+		<?php
+		$this->create_table( 'network_id', $all_networks, $current_user_network );
+	}
+
+	protected function create_table( $name, $all_data, $current_data ) {
+		$data_format = array();
+		?>
 		<div class="wrap">
 			<div class="tablenav top">
 				<div class="alignleft actions bulkactions">
-					<select name="add_site" id="action-selector-site" style="min-width: 200px;">
-						<?php if ( count( $all_sites ) > 0 ) : ?>
-							<?php foreach ( $all_sites as $site ) : ?>
-								<?php $data_site_format[ $site->blog_id ] = $site; ?>
-								<?php if ( ! in_array( $site->blog_id, $current_user_site, true ) ) : ?>
-									<option value="<?php echo esc_html( $site->blog_id ); ?>">
-										<?php echo esc_html( $site->blogname ); ?> <b>(ID: <?php echo esc_html( $site->blog_id ); ?>)</b>
+					<select type="<?php echo esc_attr( $name ); ?>" style="min-width: 200px;">
+						<?php if ( count( $all_data ) > 0 ) : ?>
+							<?php foreach ( $all_data as $data ) : ?>
+								<?php $data_format[ $data->id ] = $data; ?>
+								<?php if ( ! in_array( (string) $data->id, $current_data, true ) ) : ?>
+									<option value="<?php echo esc_attr( $data->id ); ?>">
+										<?php echo esc_html( $data->site_name ); ?> <b>(ID: <?php echo esc_html( $data->id ); ?>)</b>
 									</option>
 								<?php endif; ?>
 							<?php endforeach; ?>
-						<?php else : ?>
-							<option ><?php _e( 'Empty site', 'wp-separate-user-base' ); ?></option>
 						<?php endif; ?>
 					</select>
-					<button type="button" id="add_site" class="button action"><?php _e( 'Add', 'wp-separate-user-base' ); ?></button>
+					<button type="button" class="add_row button action"><?php esc_html_e( 'Add', 'wp-separate-user-base' ); ?></button>
 				</div>
 			</div>
-			<table id="list_site" class="wp-list-table widefat fixed striped table-view-list posts" style="width:500px;">
-				<tbody id="the-list">
-				<?php if ( count( $current_user_site ) > 0 ) : ?>
-					<?php foreach ( $current_user_site as $site_id ) : ?>
-						<tr id="site-<?php echo $site_id; ?>" >
+			<table class="wp-list-table widefat fixed striped table-view-list posts" style="width:500px;">
+				<tbody>
+				<?php if ( count( $current_data ) > 0 ) : ?>
+					<?php foreach ( $current_data as $data_id ) : ?>
+						<tr id="site-<?php echo $data_id; ?>" >
 							<td class="title column-title has-row-actions column-primary page-title" data-colname="Site">
-								<?php if ( isset( $data_site_format[ $site_id ] ) ) : ?>
-									<?php echo esc_html( $data_site_format[ $site_id ]->blogname ); ?> (ID: <?php echo esc_html( $data_site_format[ $site_id ]->blog_id ); ?>)
+								<?php if ( isset( $data_format[ $data_id ] ) ) : ?>
+									<?php echo esc_html( $data_format[ $data_id ]->site_name ); ?> (ID: <?php echo esc_html( $data_format[ $data_id ]->id ); ?>)
 								<?php else : ?>
-									Site <?php echo esc_html( $site_id ); ?>
+									Site <?php echo esc_html( $data_id ); ?>
 								<?php endif; ?>
 							</td>
 							<td class="action-remove-site" style="width: 80px;" >
-								<input class="input-text" type="hidden" value="<?php echo esc_html( $site_id ); ?>" name="site_id[<?php echo esc_html( $site_id ); ?>]" />
+								<input class="input-text" type="hidden" value="<?php echo esc_attr( $data_id ); ?>" name="<?php echo esc_attr( $name ); ?>[<?php echo esc_attr( $data_id ); ?>]" />
 								<button type="button" class="button action">
-									<span class="btn-remove-site"><?php _e( 'Remove', 'wp-separate-user-base' ); ?></span>
+									<span class="btn-remove-site"><?php esc_html_e( 'Remove', 'wp-separate-user-base' ); ?></span>
 								</button>
 							</td>
 						</tr>
 					<?php endforeach; ?>
-				<?php else : ?>
-					<tr>
-						<td colspan="2"><?php _e( 'Empty site', 'wp-separate-user-base' ); ?></td>
-					</tr>
 				<?php endif; ?>
-
 				</tbody>
 			</table>
 		</div>
 		<?php
 	}
 
+
 	/**
 	 * @param $user_id
 	 * @return void
 	 */
-	public function manage_site_options_update( $user_id ) {
+	public function manage_user_section_update( $user_id ) {
 		if ( user_can( $user_id, 'manage_network_users' ) ) {
 
-			$current_user_site = wp_sub_get_user_sites( (int) $user_id );
+			$this->manage_site_update( $user_id );
+			$this->manage_network_update( $user_id );
+		}
+	}
 
-			$site_ids = array();
-			if ( isset( $_POST['site_id'] ) ) {
-				$site_ids = $_POST['site_id'];
-			}
-			if ( ! empty( $current_user_site ) ) {
-				foreach ( $current_user_site as $site_id ) {
-					if ( ! in_array( $site_id, $site_ids, true ) ) {
-						wp_sub_remove_user_from_site( $user_id, (int) $site_id );
-					}
+	protected function manage_site_update( $user_id ) {
+		$current_user_site = wp_sub_get_user_sites( (int) $user_id );
+
+		$site_ids = array();
+		if ( isset( $_POST['site_id'] ) ) {
+			$site_ids = $_POST['site_id'];
+		}
+		if ( ! empty( $current_user_site ) ) {
+			foreach ( $current_user_site as $site_id ) {
+				if ( ! in_array( $site_id, $site_ids, true ) ) {
+					wp_sub_remove_user_from_site( $user_id, (int) $site_id );
 				}
 			}
-			if ( ! empty( $site_ids ) ) {
-				foreach ( $site_ids as $site_id ) {
-					if ( ! in_array( $site_id, $current_user_site, true ) ) {
-						wp_sub_add_user_to_site( $user_id, (int) $site_id );
-					}
+		}
+		if ( ! empty( $site_ids ) ) {
+			foreach ( $site_ids as $site_id ) {
+				if ( ! in_array( $site_id, $current_user_site, true ) ) {
+					wp_sub_add_user_to_site( $user_id, (int) $site_id );
+				}
+			}
+		}
+	}
+
+	protected function manage_network_update( $user_id ) {
+		$current_user_network = get_user_meta( $user_id, WP_Separate_User_Base::NETWORK_META_KEY, false );
+
+		$network_ids = array();
+		if ( isset( $_POST['network_id'] ) ) {
+			$network_ids = $_POST['network_id'];
+		}
+		if ( ! empty( $current_user_network ) ) {
+			foreach ( $current_user_network as $network_id ) {
+				if ( ! in_array( $network_id, $network_ids, true ) ) {
+					wp_sub_remove_user_from_network( $user_id, (int) $network_id );
+				}
+			}
+		}
+		if ( ! empty( $network_ids ) ) {
+			foreach ( $network_ids as $network_id ) {
+				if ( ! in_array( $network_id, $current_user_network, true ) ) {
+					wp_sub_add_user_to_network( $user_id, (int) $network_id );
 				}
 			}
 		}
--- includes/js/table.js
+++ includes/js/table.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const buttonAdd = document.querySelector('#add_site');
-  buttonAdd.addEventListener('click', (e) => {
-    if (e.target.id === 'add_site') {
-      const select = document.querySelector('#action-selector-site');
-      const id = document.querySelector('#list_site').querySelectorAll('tbody')[0];
+  document.addEventListener('click', (e) => {
+    if (e.target.classList && e.target.classList[0] === 'add_row') {
+      const select = e.target.parentNode.querySelector('select');
+      const id = e.target.parentNode.parentNode.parentNode.querySelector('table').querySelectorAll('tbody')[0];
       const newRow = id.insertRow();
       const selectedElement = select.options[select.selectedIndex];
-      newRow.innerHTML = `<tr id="site-${selectedElement.value}" >`
-                + `<td class="title column-title has-row-actions column-primary page-title" data-colname="Site">${
-                  selectedElement.innerHTML
-                }</td>`
-                + '<td class="action-remove-site" style="width: 80px;" >'
-                + `<input class="input-text" type="hidden" value="${selectedElement.value}" name="site_id[${selectedElement.value}]" />`
-                + ' <button type="button" class="button action"> '
-                + '<span class="btn-remove-site">Remove</span> '
-                + '</button> '
-                + '</td>'
-                + '</tr>';
-      select.remove(select.selectedIndex);
+      if (selectedElement) {
+        newRow.innerHTML = `<tr id="site-${selectedElement.value}" >`
+            + `<td class="title column-title has-row-actions column-primary page-title" data-colname="Site">${
+              selectedElement.innerHTML
+            }</td>`
+            + '<td class="action-remove-site" style="width: 80px;" >'
+            + `<input class="input-text" type="hidden" value="${selectedElement.value}" name="${select.getAttribute('type')}[${selectedElement.value}]" />`
+            + ' <button type="button" class="button action"> '
+            + '<span class="btn-remove-site">Remove</span> '
+            + '</button> '
+            + '</td>'
+            + '</tr>';
+        select.remove(select.selectedIndex);
+      }
     }
   }, false);
   document.addEventListener('click', (e) => {
     if (e.target.classList && e.target.classList[0] === 'btn-remove-site') {
       const deleteRow = e.target.parentNode.parentNode.parentNode;
-      const select = document.querySelector('#action-selector-site');
+      const select = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('select');
       const text = deleteRow.querySelectorAll('.column-title')[0].innerHTML;
       const { value } = deleteRow.querySelectorAll('.input-text')[0];
       select.options[select.options.length] = new Option(text, value);
